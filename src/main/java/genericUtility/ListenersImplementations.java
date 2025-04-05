8a7d9f61eeb24331b5c0a10f94673c5cc7fe1d4f
Rkesh <bitrakesh31@gmail.com>
2025-04-05 08:22:41 +0530

    Pushing framework

--- src/main/java/genericUtility/ListenersImplementations.java
+++ src/main/java/genericUtility/ListenersImplementations.java
@@ -6,83 +6,118 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class ListenersImplementations implements ITestListener{
-	
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ListenersImplementations implements ITestListener {
+
+	ExtentReports report;
+	ExtentTest test;
+
 	/**
-	   * Invoked before running all the test methods belonging to the classes inside the &lt;test&gt;
-	   * tag and calling all their Configuration methods.
-	   *
-	   * @param context The test context
-	   */
-	  public void onStart(ITestContext context) {
-	      System.out.println("---Suite Execution Started---");
-	  }
-	  
-	  /**
-	   * Invoked each time before a test will be invoked. The <code>ITestResult</code> is only partially
-	   * filled with the references to class, method, start millis and status.
-	   *
-	   * @param result the partially filled <code>ITestResult</code>
-	   * @see ITestResult#STARTED
-	   */
-	  public void onTestStart(ITestResult result) {
-	      String methodname=result.getMethod().getMethodName();
-	      System.out.println(methodname+"---Started---");
-	  }
-	  
-	  /**
-	   * Invoked each time a test succeeds.
-	   *
-	   * @param result <code>ITestResult</code> containing information about the run test
-	   * @see ITestResult#SUCCESS
-	   */
-	  public void onTestSuccess(ITestResult result) {
-		  String methodname=result.getMethod().getMethodName();
-	      System.out.println(methodname+"---Passed---");
-	  }
-	  
-	  /**
-	   * Invoked each time a test is skipped.
-	   *
-	   * @param result <code>ITestResult</code> containing information about the run test
-	   * @see ITestResult#SKIP
-	   */
-	  public void onTestSkipped(ITestResult result) {
-		  String methodname=result.getMethod().getMethodName();
-	      System.out.println(methodname+"---Skipped---");
-	  }
-	  
-	  /**
-	   * Invoked each time a test fails.
-	   *
-	   * @param result <code>ITestResult</code> containing information about the run test
-	   * @see ITestResult#FAILURE
-	   */
-	  public void onTestFailure(ITestResult result) {
-		  String methodname=result.getMethod().getMethodName();
-	      System.out.println(methodname+"---Failed---");
-	      
-	      WebDriverUtility wutil=new WebDriverUtility();
-	      JavaUtility jutil=new JavaUtility();
-	      
-	      String screenshotname=methodname+" "+jutil.toGetSystemDateAndTime();
-	      
-	      try {
-			wutil.toTakeSCreenshot(BaseClass.sdriver, screenshotname);
+	 * Invoked before running all the test methods belonging to the classes inside
+	 * the &lt;test&gt; tag and calling all their Configuration methods.
+	 *
+	 * @param context The test context
+	 */
+	public void onStart(ITestContext context) {
+		System.out.println("---Suite Execution Started---");
+
+		ExtentSparkReporter htmlReport = new ExtentSparkReporter(
+				"./ExtentReports/Reports-" + new JavaUtility().toGetSystemDateAndTime() + ".html");
+		htmlReport.config().setDocumentTitle("Vtiger Execution Report");
+		htmlReport.config().setTheme(Theme.DARK);
+		htmlReport.config().setReportName("VTIGER EXECUTION REPORT");
+
+		report = new ExtentReports();
+		report.attachReporter(htmlReport);
+		report.setSystemInfo("BaseUrl", "http://localhost:8888/");
+		report.setSystemInfo("username", "admin");
+		report.setSystemInfo("password", "password");
+		report.setSystemInfo("Reporter Name", "lalu");
+	}
+
+	/**
+	 * Invoked each time before a test will be invoked. The <code>ITestResult</code>
+	 * is only partially filled with the references to class, method, start millis
+	 * and status.
+	 *
+	 * @param result the partially filled <code>ITestResult</code>
+	 * @see ITestResult#STARTED
+	 */
+	public void onTestStart(ITestResult result) {
+		String methodname = result.getMethod().getMethodName();
+		System.out.println(methodname + "---Started---");
+		test = report.createTest(methodname);
+	}
+
+	/**
+	 * Invoked each time a test succeeds.
+	 *
+	 * @param result <code>ITestResult</code> containing information about the run
+	 *               test
+	 * @see ITestResult#SUCCESS
+	 */
+	public void onTestSuccess(ITestResult result) {
+		String methodname = result.getMethod().getMethodName();
+		System.out.println(methodname + "---Passed---");
+		test.log(Status.PASS, methodname + "----Passed");
+	}
+
+	/**
+	 * Invoked each time a test is skipped.
+	 *
+	 * @param result <code>ITestResult</code> containing information about the run
+	 *               test
+	 * @see ITestResult#SKIP
+	 */
+	public void onTestSkipped(ITestResult result) {
+		String methodname = result.getMethod().getMethodName();
+		System.out.println(methodname + "---Skipped---");
+		test.log(Status.SKIP, methodname + "---Skipped");
+		test.log(Status.INFO, result.getThrowable());
+	}
+
+	/**
+	 * Invoked each time a test fails.
+	 *
+	 * @param result <code>ITestResult</code> containing information about the run
+	 *               test
+	 * @see ITestResult#FAILURE
+	 */
+	public void onTestFailure(ITestResult result) {
+		String methodname = result.getMethod().getMethodName();
+		System.out.println(methodname + "---Failed---");
+		test.log(Status.FAIL, methodname + "---Failed");
+		//test.log(Status.FAIL, result.getThrowable());
+		test.log(Status.INFO, result.getThrowable());
+
+		WebDriverUtility wutil = new WebDriverUtility();
+		JavaUtility jutil = new JavaUtility();
+
+		String screenshotname = methodname + " " + jutil.toGetSystemDateAndTime();
+
+		try {
+			String path = wutil.toTakeSCreenshot(BaseClass.sdriver, screenshotname);
+			test.addScreenCaptureFromPath(path);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	  }
-	  
-	  /**
-	   * Invoked after all the test methods belonging to the classes inside the &lt;test&gt; tag have
-	   * run and all their Configuration methods have been called.
-	   *
-	   * @param context The test context
-	   */
-	  public void onFinish(ITestContext context) {
-	      System.out.println("---Suite Execution Finished---");
-	  }
+	}
+
+	/**
+	 * Invoked after all the test methods belonging to the classes inside the
+	 * &lt;test&gt; tag have run and all their Configuration methods have been
+	 * called.
+	 *
+	 * @param context The test context
+	 */
+	public void onFinish(ITestContext context) {
+		System.out.println("---Suite Execution Finished---");
+		report.flush();
+	}
 
 }
--- src/main/java/genericUtility/WebDriverUtility.java
+++ src/main/java/genericUtility/WebDriverUtility.java
@@ -17,208 +17,259 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
- * This Class contains all the methods related to Webdriver 
+ * This Class contains all the methods related to Webdriver
+ * 
  * @author Rakesh
  */
 
 public class WebDriverUtility {
 	/**
 	 * This method is used to maximize the Browser
+	 * 
 	 * @param driver
 	 */
-	
-	public void toMaximize(WebDriver driver){
-	driver.manage().window().maximize();
-	} 
-	
+
+	public void toMaximize(WebDriver driver) {
+		driver.manage().window().maximize();
+	}
+
 	/**
 	 * This methos is used to delete All cookies provided driver
+	 * 
 	 * @param driver
 	 */
 	public void toDeleteAllCookies(WebDriver driver) {
-	driver.manage().deleteAllCookies();
+		driver.manage().deleteAllCookies();
 	}
+
 	/**
 	 * This method is used to minimize the Browser
+	 * 
 	 * @param driver
 	 */
-	
-	public void toMinimize(WebDriver driver){
+
+	public void toMinimize(WebDriver driver) {
 		driver.manage().window().minimize();
-	} 
-	
+	}
+
 	/**
-	 * This method is used to Wait until the element is loaded in given time duartion provide driver
+	 * This method is used to Wait until the element is loaded in given time
+	 * duartion provide driver
+	 * 
 	 * @param driver
 	 */
-	public void toImplicitlyWait(WebDriver driver){
+	public void toImplicitlyWait(WebDriver driver) {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-	} 
+	}
+
 	/**
-	 * This method is used to wait until the element is clickable provided driver and element
+	 * This method is used to wait until the element is clickable provided driver
+	 * and element
+	 * 
 	 * @param driver
 	 * @param element
 	 */
-	public void toWaitUntilElementClickable(WebDriver driver,WebElement element) {
-		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(15));
+	public void toWaitUntilElementClickable(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
+
 	/**
-	 * This method i used wait until the element is visible provided driver and element
+	 * This method i used wait until the element is visible provided driver and
+	 * element
+	 * 
 	 * @param driver
 	 * @param element
 	 */
-	public void toWaitUntilVisibilityOfElement(WebDriver driver,WebElement element) {
-		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(15));
+	public void toWaitUntilVisibilityOfElement(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		wait.until(ExpectedConditions.visibilityOf(element));
 	}
+
 	/**
 	 * This method is used to handle dropdown using index provided element and index
+	 * 
 	 * @param element
 	 * @param index
 	 */
 	public void toHandleDropdown(WebElement element, int index) {
-		Select select=new Select(element);
+		Select select = new Select(element);
 		select.selectByIndex(index);
 	}
+
 	/**
 	 * This method is used to handle dropdown using value provided element and value
+	 * 
 	 * @param element
 	 * @param value
 	 */
 	public void toHandleDropdown(WebElement element, String value) {
-		Select select=new Select(element);
+		Select select = new Select(element);
 		select.selectByValue(value);
 	}
+
 	/**
-	 * This method is used to handle dropdown using visible text provided element and text
+	 * This method is used to handle dropdown using visible text provided element
+	 * and text
+	 * 
 	 * @param text
 	 * @param element
 	 */
-	public void toHandleDropdown(String text,WebElement element) {
-		Select select=new Select(element);
+	public void toHandleDropdown(String text, WebElement element) {
+		Select select = new Select(element);
 		select.selectByVisibleText(text);
 	}
+
 	/**
 	 * This method is use to perform mouse hover action provided driver and element
+	 * 
 	 * @param driver
 	 * @param element
 	 */
-	public void toMouseHover(WebDriver driver,WebElement element) {
-		Actions action=new Actions(driver);
+	public void toMouseHover(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
 		action.moveToElement(element).perform();
 	}
+
 	/**
 	 * This methos is used to perform right click provided driver and element
+	 * 
 	 * @param driver
 	 * @param element
 	 */
-	public void toRightClick(WebDriver driver,WebElement element) {
-		Actions action=new Actions(driver);
+	public void toRightClick(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
 		action.contextClick(element).perform();
 	}
+
 	/**
 	 * This method is used to perform double click provided webdriver and webelement
+	 * 
 	 * @param driver
 	 * @param element
 	 */
-	public void toDoubleClick(WebDriver driver,WebElement element) {
-		Actions action=new Actions(driver);
+	public void toDoubleClick(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
 		action.doubleClick(element).perform();
 	}
+
 	/**
 	 * This method is use to perform drag and drop provided Webdriver and WebElement
+	 * 
 	 * @param driver
 	 * @param src
 	 * @param target
 	 */
-	public void toDragAndDrop(WebDriver driver,WebElement src,WebElement target) {
-		Actions action=new Actions(driver);
-		action.dragAndDrop(src,target).perform();
+	public void toDragAndDrop(WebDriver driver, WebElement src, WebElement target) {
+		Actions action = new Actions(driver);
+		action.dragAndDrop(src, target).perform();
 	}
+
 	/**
 	 * This method is used to handle frame using driver and index
+	 * 
 	 * @param driver
 	 * @param index
 	 */
-	public void toHandleFrame(WebDriver driver,int index) {
+	public void toHandleFrame(WebDriver driver, int index) {
 		driver.switchTo().frame(index);
 	}
+
 	/**
 	 * This method is used to handle frame using driver and name_id
+	 * 
 	 * @param driver
 	 * @param name_id
 	 */
-	public void toHandleFrame(WebDriver driver,String name_id) {
+	public void toHandleFrame(WebDriver driver, String name_id) {
 		driver.switchTo().frame(name_id);
 	}
+
 	/**
 	 * This method is used to handle frame using driver and element
+	 * 
 	 * @param driver
 	 * @param element
 	 */
-	public void toHandleFrame(WebDriver driver,WebElement element) {
+	public void toHandleFrame(WebDriver driver, WebElement element) {
 		driver.switchTo().frame(element);
 	}
+
 	/**
 	 * This method is used to come back from frame to main page using driver
+	 * 
 	 * @param driver
 	 */
 	public void toSwitchBackFromFrame(WebDriver driver) {
 		driver.switchTo().defaultContent();
 	}
+
 	/**
 	 * This method is used to handle alert popup by accept using webdriver
+	 * 
 	 * @param driver
 	 */
 	public void toSwitchToAlertAndAccept(WebDriver driver) {
 		driver.switchTo().alert().accept();
 	}
+
 	/**
 	 * This method is used to handle alert popup by dismissing using webdriver
+	 * 
 	 * @param driver
 	 */
 	public void toSwitchToAlertAndDismiss(WebDriver driver) {
 		driver.switchTo().alert().dismiss();
 	}
+
 	/**
-	 * This method is used to capture text in alert popup and accept it provided driver 
+	 * This method is used to capture text in alert popup and accept it provided
+	 * driver
+	 * 
 	 * @param driver
 	 * @return
 	 */
 	public String toSwitchToAlertAndCaptureMesssage(WebDriver driver) {
-		Alert alertPopUp=driver.switchTo().alert();
-		String message=alertPopUp.getText();
+		Alert alertPopUp = driver.switchTo().alert();
+		String message = alertPopUp.getText();
 		alertPopUp.accept();
 		return message;
 	}
+
 	/**
-	 * This method is used to take screenshot of an entire webpage provided driver and screenshotname
+	 * This method is used to take screenshot of an entire webpage provided driver
+	 * and screenshotname
+	 * 
 	 * @param driver
 	 * @param screenshotname
 	 * @throws IOException
 	 */
-	public void toTakeSCreenshot(WebDriver driver ,String screenshotname) throws IOException {
-		TakesScreenshot ts=(TakesScreenshot)driver;
-		File temp=ts.getScreenshotAs(OutputType.FILE);
-		File src=new File("./errorShots/"+screenshotname+".png");
+	public String toTakeSCreenshot(WebDriver driver, String screenshotname) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File temp = ts.getScreenshotAs(OutputType.FILE);
+		File src = new File("./errorShots/" + screenshotname + ".png");
 		FileHandler.copy(temp, src);
+		String path = src.getAbsolutePath();
+		return path;
 	}
+
 	/**
-	 * This method is used to transfer driver control to window provided driver and partialTitle
+	 * This method is used to transfer driver control to window provided driver and
+	 * partialTitle
+	 * 
 	 * @param driver
 	 * @param partialTitle
 	 */
 	public void toSwitchWindow(WebDriver driver, String partialTitle) {
-		Set <String> allIds=driver.getWindowHandles();
-		
-		for(String id: allIds) {
-			String title=driver.switchTo().window(id).getTitle();
-			
-			if(title.contains(partialTitle)) {
+		Set<String> allIds = driver.getWindowHandles();
+
+		for (String id : allIds) {
+			String title = driver.switchTo().window(id).getTitle();
+
+			if (title.contains(partialTitle)) {
 				break;
 			}
 		}
 	}
-	
+
 }
